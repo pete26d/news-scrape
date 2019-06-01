510d9f7593064760c40a86ce1789bc8071ae8990
Spatula Pete <petewx26@gmail.com>
2019-06-01 15:54:36 -0400

    added summary to scraped list

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
 
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
@@ -26,27 +27,36 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/newScrape", { useNewUrlParser: true });
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/newScrape";
+
+// Connect to the Mongo DB
+
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
 
 // Routes
 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
-  axios.get("httphttps://www.wunderground.com/").then(function(response) {
+  axios.get("https://www.wunderground.com/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("a.hp-story-title").each(function(i, element) {
+   
+    $("div.story-body").each(function(i, element) {
       console.log(element);
       // Save an empty result object
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(element).text();
-      result.link = $(this).attr("href");
+      result.title = $(element).find("h2").find(".hp-story-title").text();
+      result.link = $(element).find("h2").find(".hp-story-title").attr("href");
+      result.summary = $(element).find(".hp-story-summary").text();
 
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
@@ -65,13 +75,28 @@ app.get("/scrape", function(req, res) {
   });
 });
 
+app.get("/", function(req, res) {
+  db.Article.find({})
+    .then(function (dbArticle) {
+      let hbsObject;
+      hbsObject = {
+          articles: dbArticle
+      };
+      res.render("index", hbsObject);
+    })
+    .catch(function (err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+    });
+});
+
 // Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
   // Grab every document in the Articles collection
   db.Article.find({})
     .then(function(dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
-      res.json(dbArticle);
+      res.render(dbArticle);
     })
     .catch(function(err) {
       // If an error occurred, send it to the client
